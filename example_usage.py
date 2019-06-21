094070f9509edf0236c0a993e407af63b0880522
Paul Pauls <mail@paulpauls.de>
2019-06-21 15:14:13 +0200

    Formatting

--- example_usage.py
+++ example_usage.py
@@ -11,15 +11,12 @@ def main():
     logger.debug(tf.__version__)
 
     env = CartPoleEnvironment()
-
     config = neuroevolution.Config(neuroevolution.algorithms.YANA, './yana_example_config.cfg')
-
     pop = neuroevolution.Population(config)
 
-    engine = neuroevolution.EvolutionEngine(config, pop, env.eval_genome_fitness)
+    engine = neuroevolution.EvolutionEngine(config, pop, env)
 
     best_genome = engine.train(max_generations=100)
-
     env.replay_genome(best_genome)
 
 
