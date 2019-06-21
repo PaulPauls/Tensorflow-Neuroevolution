e6f7c9006cb4111e86812c74d906ba4392ebf063
Paul Pauls <mail@paulpauls.de>
2019-06-21 12:39:51 +0200

    outsource training environment from example usage to proper environment

--- example_usage.py
+++ example_usage.py
@@ -1,38 +1,9 @@
-import gym
 import logging
 import tensorflow as tf
+
 import neuroevolution
 
-from neuroevolution.environments import BaseEnvironment
-
-
-class Environment(BaseEnvironment):
-    """
-    ToDo: Implement possibility that an algorithm will require multiple test environments either due to parallel
-          execution (see batch_size) or the inability of the environment to be properly reset when testing a new genome.
-          Therefore possibly put the creation of this class in the evolution_engine as it will know the batch_size.
-    """
-    def __init__(self):
-        """
-        ToDo
-        """
-        self.env = gym.make('CartPole-v1')
-
-    def eval_genome_fitness(self, genome):
-        """
-        ToDo: Input genome; apply the genome to the test environments; Return its calculated resulting fitness value
-        :param genome:
-        :return:
-        """
-        pass
-
-    def replay_genome(self, genome):
-        """
-        ToDo: Input genome, apply it to the test environment, though this time render the process of it being applied
-        :param genome:
-        :return: None
-        """
-        pass
+from neuroevolution.environments.opengym.cartpole_environment import CartPoleEnvironment
 
 
 if __name__ == '__main__':
@@ -41,7 +12,7 @@ if __name__ == '__main__':
 
     print(tf.__version__)
 
-    env = Environment()
+    env = CartPoleEnvironment()
 
     config = neuroevolution.Config(neuroevolution.algorithms.YANA, 'Path/to/config')
 
