02897b3453a9f3c54d8a0c4208a35036d53fcc6c
Paul Pauls <mail@paulpauls.de>
2019-06-22 13:14:26 +0200

    Add functionality to load specified configuration

--- neuroevolution/config.py
+++ neuroevolution/config.py
@@ -1,4 +1,6 @@
+import os
 import tensorflow as tf
+from configparser import ConfigParser
 
 
 class Config:
@@ -12,13 +14,28 @@ class Config:
         """
         self.logger = tf.get_logger()
 
-        self.algorithm_parameters = self._load_algorithm_parameters()
+        self.algorithm_parameters = self._load_algorithm_parameters(config_path)
+        self.logger.debug(self.algorithm_parameters)
 
-        pass
-
-    def _load_algorithm_parameters(self):
+    @staticmethod
+    def _load_algorithm_parameters(config_path):
         """
         ToDo
+        :param config_path:
         :return:
         """
-        pass
+
+        if not os.path.isfile(config_path):
+            raise Exception("Specified configuration file does not exist: " + os.path.abspath(config_path))
+
+        parameters = ConfigParser()
+        with open(config_path) as config_file:
+            parameters.read_file(config_file)
+
+        print(parameters.sections())
+
+        algorithm_parameters = {}
+        for section in parameters.sections():
+            algorithm_parameters[section] = parameters.items(section)
+
+        return algorithm_parameters
