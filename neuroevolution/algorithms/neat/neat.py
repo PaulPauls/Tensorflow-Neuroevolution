23c8a5c2e08dfddce221df0cc48dfabfb9184903
Paul Pauls <mail@paulpauls.de>
2019-06-26 16:02:35 +0200

    Minimize Neat example as long as only stub

--- neuroevolution/algorithms/neat/neat.py
+++ neuroevolution/algorithms/neat/neat.py
@@ -3,39 +3,11 @@ from neuroevolution.algorithms.base_algorithm import BaseNeuroevolutionAlgorithm
 
 class NEAT(BaseNeuroevolutionAlgorithm):
     """
-    ToDo:
+    ToDo
     """
     def __init__(self, *args):
         """
         ToDo
         :param args:
-        """
-        pass
-
-    def create_genomes(self):
-        """
-        ToDo
-        :return:
-        """
-        pass
-
-    def select_genomes(self):
-        """
-        ToDo
-        :return:
-        """
-        pass
-
-    def mutate_genomes(self):
-        """
-        ToDo
-        :return:
-        """
-        pass
-
-    def speciate_genomes(self):
-        """
-        ToDo
-        :return:
         """
         pass
