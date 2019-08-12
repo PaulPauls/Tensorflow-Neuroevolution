e02f4203685ce4929721ced97967a1938b3aa8f3
Paul Pauls <mail@paulpauls.de>
2019-08-12 22:24:25 +0200

    Generalize population class to make suitable for different genomes

--- neuroevolution/population.py
+++ neuroevolution/population.py
@@ -1,6 +1,52 @@
+from collections import deque
+
 import tensorflow as tf
 
 
+class Population:
+    def __init__(self, encoding):
+        self.logger = tf.get_logger()
+
+        self.encoding = encoding
+
+        self.genomes = deque(maxlen=None)
+        self.generation_counter = None
+
+    def create_initial_population(self):
+        raise NotImplementedError("create_initial_population() not yet implemented")
+
+    def load_population(self):
+        raise NotImplementedError("load_population() not yet implemented")
+
+    def save_population(self):
+        raise NotImplementedError("save_population() not yet implemented")
+
+    def add_genome(self, genome):
+        self.genomes.append(genome)
+
+    def remove_genome(self, genome):
+        self.genomes.remove(genome)
+
+    def increment_generation_counter(self):
+        self.generation_counter += 1
+
+    def check_extinction(self):
+        return len(self.genomes) == 0
+
+    def get_genome(self, i):
+        return self.genomes[i]
+
+    def get_best_genome(self):
+        return max(self.genomes, key=lambda x: x.get_fitness())
+
+    def get_worst_genome(self):
+        return min(self.genomes, key=lambda x: x.get_fitness())
+
+    def get_generation_counter(self):
+        return self.generation_counter
+
+
+'''
 class Population:
     def __init__(self, encoding):
         self.logger = tf.get_logger()
@@ -47,3 +93,4 @@ class Population:
 
     def load_population(self):
         raise NotImplementedError("load_population() not yet implemented")
+'''
