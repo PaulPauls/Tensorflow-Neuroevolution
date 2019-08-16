a138028cafde86321c7fff14acac7d774841310e
Paul Pauls <mail@paulpauls.de>
2019-08-16 13:35:34 +0200

    Adjust test_xor example to current version of DirectEncoding

--- tests/test_xor.py
+++ tests/test_xor.py
@@ -7,23 +7,20 @@ import neuroevolution as ne
 def test_xor():
     assert tf.__version__ == "2.0.0-beta1"
 
-    genome_prototype = {
-        0: {
-            'inputs': [1, 4],
-            'outputs': [2],
-            'layer_activations': [tf.keras.activations.tanh],
-            'out_activation': tf.keras.activations.sigmoid,
-            'default_activation': tf.keras.activations.sigmoid
-        },
+    genotype = {
         1: (1, 2),
         2: (1, 3),
         3: (4, 3),
         4: (4, 2),
-        5: (3, 2),
-    }
-    print("Custom Genotype: {}".format(genome_prototype))
+        5: (3, 2)}
 
-    genome = ne.encodings.direct.DirectEncodingGenome(1, genome_prototype, check_genome_sanity=True)
+    activations = {'out_activation': tf.keras.activations.sigmoid,
+                   'default_activation': tf.keras.activations.sigmoid}
+
+    config = ne.load_config('../examples/xor_example/example_yanaAlg_directEnc_xorEnv.cfg')
+    encoding = ne.encodings.DirectEncoding(config)
+
+    genome = encoding.create_new_genome(genotype, activations, check_genome_sanity=True)
     genome.summary()
     genome.visualize()
 
