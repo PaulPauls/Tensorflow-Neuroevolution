682509a215c8f0bec99b5d9656078bf49aabfc68
Paul Pauls <mail@paulpauls.de>
2019-09-12 15:57:21 +0200

    Reset base classes for now in order to reimplement later

--- neuroevolution/encodings/base_encoding.py
+++ neuroevolution/encodings/base_encoding.py
@@ -2,6 +2,4 @@ from abc import ABCMeta, abstractmethod
 
 
 class BaseEncoding(object, metaclass=ABCMeta):
-    @abstractmethod
-    def create_new_genome(self, genotype, *args, **kwargs):
-        raise NotImplementedError("Should implement create_new_genome()")
+    pass
