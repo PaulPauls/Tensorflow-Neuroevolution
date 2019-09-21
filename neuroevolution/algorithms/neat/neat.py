42f8858ad4006e42a480eef9c6e518b469d83566
Paul Pauls <mail@paulpauls.de>
2019-09-21 20:46:26 +0200

    Implement neat config read-out of the new speciation parameters for the NEAT algorithm

--- neuroevolution/algorithms/neat/neat.py
+++ neuroevolution/algorithms/neat/neat.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from absl import logging
 
 from ..base_algorithm import BaseNeuroevolutionAlgorithm
@@ -8,19 +9,24 @@ class NEAT(BaseNeuroevolutionAlgorithm):
         self.encoding = encoding
 
         # Declare and read in config parameters for the NEAT NE algorithm
-        self.replacement_percentage = None
-        self.mutate_prob = None
+        self.genome_elitism = None
         self.recombine_prob = None
         self.mutate_weights_prob = None
-        self.mutate_connection_prob = None
-        self.mutate_node_prob = None
+        self.add_conn_prob = None
+        self.add_node_prob = None
+        self.initial_connection = None
+        self.species_elitism = None
+        self.species_min_size = None
+        self.species_max_size = None
+        self.species_max_stagnation = None
+        self.species_max_fallback = None
+        self.species_clustering = None
+        self.species_interbreeding = None
         self.activation_default = None
         self.activation_out = None
         self._read_config_parameters(config)
 
-        # Check if mutate/recombine and different mutate probabilties are correct set and add up to 1
-        assert self.mutate_prob + self.recombine_prob == 1.0
-        assert self.mutate_weights_prob + self.mutate_connection_prob + self.mutate_node_prob == 1.0
+        assert self.recombine_prob + self.mutate_weights_prob + self.add_conn_prob + self.add_node_prob == 1.0
 
         # As NEAT evolves model weights manually, disable automatic weight training
         self.trainable = False
@@ -29,23 +35,46 @@ class NEAT(BaseNeuroevolutionAlgorithm):
         section_name_algorithm = 'NEAT' if config.has_section('NEAT') else 'NE_ALGORITHM'
         section_name_evolvable_encoding = 'DIRECT_ENCODING_EVOLVABLE' \
             if config.has_section('DIRECT_ENCODING_EVOLVABLE') else 'ENCODING_EVOLVABLE'
-        self.replacement_percentage = config.getfloat(section_name_algorithm, 'replacement_percentage')
-        self.mutate_prob = config.getfloat(section_name_algorithm, 'mutate_prob')
+
+        self.genome_elitism = config.getint(section_name_algorithm, 'genome_elitism')
         self.recombine_prob = config.getfloat(section_name_algorithm, 'recombine_prob')
         self.mutate_weights_prob = config.getfloat(section_name_algorithm, 'mutate_weights_prob')
-        self.mutate_connection_prob = config.getfloat(section_name_algorithm, 'mutate_connection_prob')
-        self.mutate_node_prob = config.getfloat(section_name_algorithm, 'mutate_node_prob')
+        self.add_conn_prob = config.getfloat(section_name_algorithm, 'add_conn_prob')
+        self.add_node_prob = config.getfloat(section_name_algorithm, 'add_node_prob')
+        self.initial_connection = config.get(section_name_algorithm, 'initial_connection')
+        self.species_elitism = config.getint(section_name_algorithm, 'species_elitism')
+        self.species_min_size = config.getint(section_name_algorithm, 'species_min_size')
+        self.species_max_size = config.getint(section_name_algorithm, 'species_max_size')
+        self.species_max_stagnation = config.getint(section_name_algorithm, 'species_max_stagnation')
+        self.species_max_fallback = config.getfloat(section_name_algorithm, 'species_max_fallback')
+        self.species_clustering = config.get(section_name_algorithm, 'species_clustering')
+        self.species_interbreeding = config.getboolean(section_name_algorithm, 'species_interbreeding')
         self.activation_default = config.get(section_name_evolvable_encoding, 'activation_default')
         self.activation_out = config.get(section_name_evolvable_encoding, 'activation_out')
 
-        logging.debug("NEAT NE Algorithm read from config: replacement_percentage = {}"
-                      .format(self.replacement_percentage))
-        logging.debug("NEAT NE Algorithm read from config: mutate_prob = {}".format(self.mutate_prob))
+        if ',' in self.species_clustering:
+            species_clustering_alg = self.species_clustering[:self.species_clustering.find(',')]
+            species_clustering_val = float(self.species_clustering[self.species_clustering.find(',') + 2:])
+            self.species_clustering = (species_clustering_alg, species_clustering_val)
+
+        self.activation_default = tf.keras.activations.deserialize(self.activation_default)
+        self.activation_out = tf.keras.activations.deserialize(self.activation_out)
+
+        logging.debug("NEAT NE Algorithm read from config: genome_elitism = {}".format(self.genome_elitism))
         logging.debug("NEAT NE Algorithm read from config: recombine_prob = {}".format(self.recombine_prob))
         logging.debug("NEAT NE Algorithm read from config: mutate_weights_prob = {}".format(self.mutate_weights_prob))
-        logging.debug("NEAT NE Algorithm read from config: mutate_connection_prob = {}"
-                      .format(self.mutate_connection_prob))
-        logging.debug("NEAT NE Algorithm read from config: mutate_node_prob = {}".format(self.mutate_node_prob))
+        logging.debug("NEAT NE Algorithm read from config: add_conn_prob = {}".format(self.add_conn_prob))
+        logging.debug("NEAT NE Algorithm read from config: add_node_prob = {}".format(self.add_node_prob))
+        logging.debug("NEAT NE Algorithm read from config: initial_connection = {}".format(self.initial_connection))
+        logging.debug("NEAT NE Algorithm read from config: species_elitism = {}".format(self.species_elitism))
+        logging.debug("NEAT NE Algorithm read from config: species_min_size = {}".format(self.species_min_size))
+        logging.debug("NEAT NE Algorithm read from config: species_max_size = {}".format(self.species_max_size))
+        logging.debug("NEAT NE Algorithm read from config: species_max_stagnation = {}"
+                      .format(self.species_max_stagnation))
+        logging.debug("NEAT NE Algorithm read from config: species_max_fallback = {}".format(self.species_max_fallback))
+        logging.debug("NEAT NE Algorithm read from config: species_clustering = {}".format(self.species_clustering))
+        logging.debug("NEAT NE Algorithm read from config: species_interbreeding = {}"
+                      .format(self.species_interbreeding))
         logging.debug("NEAT NE Algorithm read from config: activation_default = {}".format(self.activation_default))
         logging.debug("NEAT NE Algorithm read from config: activation_out = {}".format(self.activation_out))
 
